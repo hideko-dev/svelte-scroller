ad038803acab8f4ce51e5fb4fd72c64ce04e0d23
Hideko <hideko332200@gmail.com>
2023-11-05 00:00:50 +0900

    fix es6 style

--- index.js
+++ index.js
@@ -1,41 +1,37 @@
-import {get, writable} from "svelte/store";
+import { get, writable } from "svelte/store";
 
-module.exports = {
+export function detectBetween({ start, end }) {
+    const status = writable(false);
+    const handleScroll = () => {
+        const scrollY = window.scrollY;
+        if (scrollY >= start && scrollY <= end) {
+            status.set(true);
+        } else {
+            status.set(false);
+        }
+    };
+    window.addEventListener("scroll", handleScroll);
+    handleScroll();
+    return get(status);
+}
 
-    detectBetween: function({ start, end }){
-        const status = writable(false);
-        const handleScroll = () => {
-            const scrollY = window.scrollY;
-            if (scrollY >= start && scrollY <= end) {
+export function detectSingle({ type, position }) {
+    const status = writable(false);
+    const handleScroll = () => {
+        const scrollY = window.scrollY;
+        if (type === "set") {
+            if (scrollY > position) {
+                status.set(true);
+            }
+        } else if (type === "bool") {
+            if (scrollY > position) {
                 status.set(true);
             } else {
                 status.set(false);
             }
-        };
-        window.addEventListener("scroll", handleScroll);
-        handleScroll();
-        return get(status);
-    },
-
-    detectSingle: function({ type, position }) {
-        const status = writable(false);
-        const handleScroll = () => {
-            const scrollY = window.scrollY;
-            if(type === "set") {
-                if (scrollY > position) {
-                    status.set(true);
-                }
-            } else if(type === "bool") {
-                if (scrollY > position) {
-                    status.set(true);
-                } else {
-                    status.set(false);
-                }
-            }
-        };
-        window.addEventListener("scroll", handleScroll);
-        handleScroll();
-        return get(status);
-    }
-
+        }
+    };
+    window.addEventListener("scroll", handleScroll);
+    handleScroll();
+    return get(status);
 }
